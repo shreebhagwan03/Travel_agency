6d7aa5983ab5d1dd00e65434da647a57d6b37f0c
shreebhagwan03 <rankawatshree@gmail.com>
2024-08-28 11:41:33 +0530

    shree commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// JavaScript for simple form validation and other interactive features
+document.querySelector('form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Message sent successfully!');
+});
